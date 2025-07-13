4cf7401dcb92391fd28ebcdbc12a53e884ec7ae2
Ajones827 <ajones158@ggc.edu>
2025-07-13 17:34:49 -0400

    Update ExitDB.java

--- 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/model/ExitDB.java
+++ 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/model/ExitDB.java
@@ -1,49 +1,47 @@
 /**Class: ExitDB.java
- * @author Kyle Williams
- * @version 1.0
- * Course: ITEC 3860, Spring 2022
- * Written: 04/15 - 04/16
+ * @author Sidibaba Simpara, Malik Key, Britney Familia, Angelica Jones
+ * Course: Course: ITEC 3860, Summer 2025
  *
  *
- * This class get the needed information in regards to exits from the database.
+ *
+ * 
  *
  */
 
 package model;
 
-import controller.Exit;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class ExitDB {
+public class ExitDatabase {
 
+    public static HashMap<Integer, ArrayList<Exit>> loadExits(String filename) {
+        HashMap<Integer, ArrayList<Exit>> exitMap = new HashMap<>();
 
-    public ArrayList<Exit> currRoomExitOptions(int currRoom) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
 
-        ArrayList<Exit> exitArrayList = new ArrayList<>();
+            while ((line = br.readLine()) != null) {
+                if (line.trim().isEmpty()) continue;
 
-        try {
-            SQLiteDB sdb = new SQLiteDB();
+                String[] parts = line.split("\\|");
+                int roomID = Integer.parseInt(parts[0].trim());
+                String direction = parts[1].trim();
+                int destinationID = Integer.parseInt(parts[2].trim());
 
-            String sql = "SELECT EXIT.direction, EXIT.destination" +
-                         " FROM EXIT" +
-                         " INNER JOIN ROOM ON EXIT.roomID=ROOM.roomNumber" +
-                         " WHERE ROOM.roomNumber = " + currRoom;
-            ResultSet rs = sdb.queryDB(sql);
-            while(rs.next()) {
-                Exit exit = new Exit();
-                exit.setDirection(rs.getString("direction"));
-                exit.setDestination(rs.getInt("destination"));
-                exitArrayList.add(exit);
+                Exit exit = new Exit(direction, destinationID);
+
+                exitMap.computeIfAbsent(roomID, k -> new ArrayList<>()).add(exit);
             }
-            sdb.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+
+        } catch (IOException e) {
+            System.out.println("Error reading exit file: " + e.getMessage());
         }
-        return exitArrayList;
+
+        return exitMap;
     }
 }
+
