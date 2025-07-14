f4bc289c161fb47526bbdda6f566d23bdbd16fcb
Ajones827 <ajones158@ggc.edu>
2025-07-13 20:51:22 -0400

    Update Exit.java

--- 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/controller/Exit.java
+++ 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/controller/Exit.java
@@ -1,66 +1,36 @@
 /**Class: Exit
- * @author Kyle Williams, Rick Price, Bradley Iverson, Miranda Darlington
- * @version 1.0
- * Course: ITEC 3860, Spring 2022
- * Written: 02/18 - 03/11
+ * @author Sidibaba Simpara, Malik Key, Britney Familia, Angelica Jones
+ * Course: Course: ITEC 3860, Summer 2025
  *
  *
- * This class acts as the blueprint for which Exit object is based on.
+ *
+ *
  *
  */
 
 package controller;
 
-import gameExceptions.GameException;
 
 public class Exit {
-
-    // Attributes
-    private int destination;
     private String direction;
-    //private final List<String> VALID_DIRECTIONS = Commands.VALID_DIRECTIONS;
+    private int destinationID;
 
-    /**
-     * no arg constructor for Exit class
-     */
-    public Exit() {
-
+    public Exit(String direction, int destinationID) {
+        this.direction = direction;
+        this.destinationID = destinationID;
     }
 
-    /**
-     * @return direction
-     */
     public String getDirection() {
         return direction;
     }
 
-    /**
-     * @param direction
-     */
-    public void setDirection(String direction) {
-        this.direction = direction;
+    public int getDestinationID() {
+        return destinationID;
     }
 
-    /**
-     * @return destination
-     */
-    public int getDestination() { return destination; }
-
-    /**
-     * @param destination
-     */
-    public void setDestination(int destination) {
-        this.destination = destination;
-    }
-
-    /**
-     * @return String version of this exit object.
-     */
     @Override
     public String toString() {
-        return "Exit{" +
-                "destination=" + destination +
-                ", direction='" + direction + '\'' +
-                '}';
+        return direction + " → Room " + destinationID;
     }
 }
+
