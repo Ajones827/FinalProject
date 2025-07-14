5776c40bea5d4229294f032b69748b3831ff4b43
Ajones827 <ajones158@ggc.edu>
2025-07-13 20:48:09 -0400

    Update Room.java

--- 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/controller/Room.java
+++ 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/controller/Room.java
@@ -11,170 +11,70 @@
 
 package controller;
 
-import gameExceptions.GameException;
-//import model.ItemDB;
-import model.ItemDB;
-import model.RoomDB;
-
-import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Room {
-
-    // Attributes
-    private String description;
-    private ArrayList<Exit> exits;
-    private ArrayList<Integer> items = new ArrayList<>();
-    private String name;
     private int roomID;
+    private String roomName;
+    private String biome;
+    private String roomDescription;
     private boolean visited;
-    private ArrayList<Puzzle> roomPuzzle = new ArrayList<>();
+    private ArrayList<Exit> exits;
 
-    /**
-     * @throws GameException
-     * @purpose no arg constructor
-     */
-    public Room() throws GameException {
-
-    }
-
-    /**
-     * @param id
-     * @throws GameException
-     * @purpose constructor that only assigns the id.
-     */
-    public Room (int id) throws GameException {
-        this.roomID = id;
-    }
-
-    /**
-     * @throws GameException
-     * @purpose replaces the old room in the database with the new one or "this" room.
-     */
-    public void updateRoom() throws GameException, SQLException, ClassNotFoundException {
-
-        RoomDB roomDB = new RoomDB();
-        roomDB.updateRoom(roomID);
-    }
-
-    /**
-     * @return build
-     * @purpose build a string representation of all the exit object associated with this room.
-     */
-    public String displayExits () {
-
-        String build = "";
-
-        for (Exit exit : this.getExits()){
-            build += "Exit Direction:  " + "[" + exit.getDirection() + "] | ";
-        }
-
-        return build;
-    }
-
-    /**
-     * @param cmd
-     * @return exit.getDestination
-     * @throws GameException
-     * @purpose tests if the user entered in a direction that is valid for the room they're currently in.
-     */
-    public int validDirection(String cmd) throws GameException {
-
-        for (Exit exit : this.exits) {
-            if (cmd.equalsIgnoreCase(exit.getDirection())){
-                return exit.getDestination();
-            }
-        }
-        return 0;
-    }
-
-    /**
-     * @return currItemList
-     * @throws GameException
-     * @purpose Get's the list of items that exist in the current room object.
-     */
-    public ArrayList<Item> getRoomItems(int playerID) throws GameException, SQLException, ClassNotFoundException {
-        ItemDB itemDB = new ItemDB();
-
-        ArrayList<Item> itemArrayList;
-        itemArrayList = itemDB.getRoomItems(roomID, playerID);
-
-        return itemArrayList;
-    }
-
-    public void removeItem(int itemID, int playerID) throws SQLException, GameException, ClassNotFoundException {
-        ItemDB itemDB = new ItemDB();
-        itemDB.removeItem(itemID, playerID);
-    }
-
-    // Getters, setters, and toString for Room from here on.
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public ArrayList<Exit> getExits() {
-        return exits;
-    }
-
-    public void setExits(ArrayList<Exit> exits) {
+    public Room(int roomID, String roomName, String biome, String roomDescription, boolean visited, ArrayList<Exit> exits) {
+        this.roomID = roomID;
+        this.roomName = roomName;
+        this.biome = biome;
+        this.roomDescription = roomDescription;
+        this.visited = visited;
         this.exits = exits;
     }
 
-    public ArrayList<Integer> getItems() {
-        return items;
-    }
-
-    public void setItems(ArrayList<Integer> items) {
-
-            this.items = items;
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Room() {
+        this.exits = new ArrayList<>();
     }
 
     public int getRoomID() {
         return roomID;
     }
 
-    public void setRoomID(int roomID) {
-        this.roomID = roomID;
+    public String getRoomName() {
+        return roomName;
     }
 
-    public boolean isVisited() throws SQLException, ClassNotFoundException {
-        RoomDB roomDB = new RoomDB();
-        int result = roomDB.isVisited(roomID);
-        if (result == 0) {
-            return false;
-        } else {
-            return true;
-        }
+    public String getBiome() {
+        return biome;
+    }
+
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    public boolean hasVisited() {
+        return visited;
     }
 
     public void setVisited(boolean visited) {
         this.visited = visited;
     }
 
+    public List<Exit> getExits() {
+        return exits;
+    }
+
+    public void addExit(Exit e) {
+        exits.add(e);
+    }
+
     @Override
     public String toString() {
-        return "Room #" + roomID + ": " + name + "\n" + description;
-    }
-
-    public ArrayList<Puzzle> getRoomPuzzle() {
-        return roomPuzzle;
-    }
-
-    public void setRoomPuzzle(ArrayList<Puzzle> roomPuzzle) {
-        this.roomPuzzle = roomPuzzle;
+        return "ROOM-ID: " + roomID +
+                "\nROOM-NAME: " + roomName +
+                "\nBIOME: " + biome +
+                "\nDESCRIPTION: " + roomDescription +
+                "\nVISITED: " + visited +
+                "\nEXITS: " + exits + "\n";
     }
 }
+
