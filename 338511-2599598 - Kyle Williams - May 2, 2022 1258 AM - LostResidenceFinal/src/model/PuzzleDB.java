1e6d5dc108a09cf2b488a81ae1622bc9f1c6e8d7
Sidibaba23 <142639349+Sidibaba23@users.noreply.github.com>
2025-07-13 21:49:46 -0400

    Update PuzzleDB.java

--- 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/model/PuzzleDB.java
+++ 338511-2599598 - Kyle Williams - May 2, 2022 1258 AM - LostResidenceFinal/src/model/PuzzleDB.java
@@ -1,9 +1,7 @@
 /**Class: PlayerDB.java
- * @author Bradley Iverson
- * @updated Kyle Williams
+ * @author Sidibaba Simpara
  * @version 1.0
- * course: ITEC 3860 Spring 2022
- * Written: Apr 6 - Apr 27, 2022
+ * course: ITEC 3860 Summer 2025
  *
  *This class updates and queries the database in regards to the puzzle functionality.
  **/
